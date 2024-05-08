e8a8c5dd956efd5bcbbb74ae6fb01cdc4304a130
keremduman30 <berwari30_1907@hotmail.com>
2024-05-08 13:30:51 +0300

    featured products improved

---
+++ src/services/fake_data.ts
@@ -0,0 +1,79 @@
+export type CardItem = {
+  id: number;
+  img1?: string;
+  img: string;
+  title: string;
+  isNew?: boolean;
+  oldPrice: number;
+  price: number;
+};
+
+const featuresdata: CardItem[] = [
+  {
+    id: 1,
+    img1: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto-compress&cs=tinysrgb&w=1600",
+    img: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "Long Sleeve Graphic T-shirt",
+    isNew: true,
+    oldPrice: 19,
+    price: 12,
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto-compress&cs=tinys rgb&w=1600",
+    title: "Coat",
+    isNew: true,
+    oldPrice: 19,
+    price: 12,
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/1457983/pexels-photo-1457983.jpeg?auto-compress&cs=tinys rgb&w=1600",
+    title: "Skirt",
+    oldPrice: 19,
+    price: 12,
+  },
+  {
+    id: 4,
+    img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto-compress&cs=tinys rgb&w=1600",
+    title: "Hat",
+    oldPrice: 19,
+    price: 12,
+  },
+];
+
+const productData: CardItem[] = [
+  {
+    id: 1,
+    img1: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto-compress&cs=tinysrgb&w=1600",
+    img: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "Coat",
+    isNew: true,
+    oldPrice: 19,
+    price: 12,
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto-compress&cs=tinys rgb&w=1600",
+    title: "Hat",
+    isNew: true,
+    oldPrice: 19,
+    price: 12,
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/1457983/pexels-photo-1457983.jpeg?auto-compress&cs=tinys rgb&w=1600",
+    title: "Skirt",
+    oldPrice: 19,
+    price: 12,
+  },
+  {
+    id: 4,
+    img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto-compress&cs=tinys rgb&w=1600",
+    title: "Hat",
+    oldPrice: 19,
+    price: 12,
+  },
+];
+
+export { featuresdata, productData };
